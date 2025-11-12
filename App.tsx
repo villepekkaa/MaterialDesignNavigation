d8b5af2ed68fbb661d8f495de671c1251cfa4f19
alavuvi <ville-pekka.alavuotunki@proton.me>
2025-11-12 12:12:11 +0200

    tehtävä valmis

--- App.tsx
+++ App.tsx
@@ -2,18 +2,27 @@ import 'react-native-gesture-handler';
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+import { Provider as PaperProvider } from 'react-native-paper';
 import HomeScreen from './screens/HomeScreen';
 import SecondScreen from './screens/SecondScreen';
+import CustomAppBar from './components/CustomAppBar';
 
 const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home">
-        <Stack.Screen name="Home" component={HomeScreen} />
-        <Stack.Screen name="SecondScreen" component={SecondScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <PaperProvider>
+      <NavigationContainer>
+        <Stack.Navigator
+          initialRouteName="Home"
+          screenOptions={{
+            header: (props) => <CustomAppBar {...props} />,
+          }}
+        >
+          <Stack.Screen name="Home" component={HomeScreen} />
+          <Stack.Screen name="Second" component={SecondScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </PaperProvider>
   );
 }
---
+++ components/CustomAppBar.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Appbar } from 'react-native-paper';
+import { StackHeaderProps } from '@react-navigation/stack';
+
+export default function CustomAppBar({ navigation, back }: StackHeaderProps) {
+  return (
+    <Appbar.Header>
+      {back ? (
+        <Appbar.Action icon="arrow-left" onPress={() => navigation.goBack()} />
+      ) : null}
+      <Appbar.Content title="Material Design Navigation" />
+      {!back && (
+        <Appbar.Action 
+          icon="arrow-right" 
+          onPress={() => navigation.navigate('Second')} 
+        />
+      )}
+    </Appbar.Header>
+  );
+}
